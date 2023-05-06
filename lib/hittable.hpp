32f475090654be5761c1195cc5a211dbd88dd355
Quinnsicle <qtoconnor@gmail.com>
2023-05-06 17:37:32 -0500

    metal material

--- lib/hittable.hpp
+++ lib/hittable.hpp
@@ -1,11 +1,15 @@
 #ifndef HITTABLE_HPP
 #define HITTABLE_HPP
 
+#include "common.hpp"
 #include "ray.hpp"
+
+class material;
 
 struct hit_record {
   point3 p;
   vec3 normal;
+  shared_ptr<material> mat_ptr;
   double t;
   bool front_face;
 
