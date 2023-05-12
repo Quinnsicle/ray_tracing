4216cb7f5189ae1ba737d952e55804109b02f6f0
Quinnsicle <qtoconnor@gmail.com>
2023-05-12 15:57:56 -0500

    final scene

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -32,11 +32,57 @@ color ray_color(const ray& r, const hittable& world, int depth) {
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+  hittable_list world;
+
+  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      auto choose_material = random_double();
+      point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+      if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+        shared_ptr<material> sphere_material;
+
+        if (choose_material < 0.8) {
+          // diffuse
+          auto albedo = color::random() * color::random();
+          sphere_material = make_shared<lambertian>(albedo);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        } else if (choose_material < 0.95) {
+          // metal
+          auto albedo = color::random(0.5, 1);
+          auto fuzz = random_double(0, 0.5);
+          sphere_material = make_shared<metal>(albedo, fuzz);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+
+        } else {
+          // glass
+          sphere_material = make_shared<dielectric>(1.5);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        }
+      }
+    }
+  }
+
+  auto material1 = make_shared<dielectric>(1.5);
+  world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+  auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+  world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+  auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+  world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+  return world;
+}
+
 int main() {
   // Image
-
   const auto aspect_ratio = 16.0 / 9.0;
-  const int image_width = 400;
+  const int image_width = 1280;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
   jpg jpg_image(image_width, image_height);
   std::vector<color> pixels;
@@ -44,34 +90,19 @@ int main() {
   const int max_depth = 50;
 
   // World
-  auto R = std::cos(pi / 4);
-  hittable_list world;
-
-  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-  auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-  auto material_left = make_shared<dielectric>(1.5);
-  auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-  world.add(
-      make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-  world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-  // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45,
-  // material_left));
-  world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+  hittable_list world = random_scene();
 
   // Camera
-  point3 look_from(3, 3, 2);
-  point3 look_at(0, 0, -1);
+  point3 look_from(13, 2, 3);
+  point3 look_at(0, 0, 0);
   vec3 up(0, 1, 0);
   auto distance_to_focus = (look_from - look_at).length();
-  auto aperture = 2.0;
+  auto aperture = 0.1;
 
   camera cam(look_from, look_at, up, 20, aspect_ratio, aperture,
              distance_to_focus);
 
   // Render
-
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanLines remaining: " << j << std::flush;
     for (int i = 0; i < image_width; ++i) {
